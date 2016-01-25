58fb33b71ea13a877f085e79f88edc7073469548
Yuxuan Li <liyuxuanzju@gmail.com>
2016-01-25 10:24:54 -0800

    Create about.java

---
+++ about.java
@@ -0,0 +1,16 @@
+package com.yuxuanli.weatherforecastinandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by LiYuxuan on 11/16/15.
+ */
+public class about extends Activity {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+    }
+}
